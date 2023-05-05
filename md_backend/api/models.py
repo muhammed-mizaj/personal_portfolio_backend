f25e0164f5ed8ff6677af2177741b56c0c9cd123
muhammed-mizaj <mdprgamer@gmail.com>
2023-05-05 15:37:54 +0530

    added some projects

--- md_backend/api/models.py
+++ md_backend/api/models.py
@@ -12,11 +12,14 @@ class Stack(models.Model):
 class Project(models.Model):
     name=models.CharField(max_length=100)
     image=models.ImageField(default='code.jpg',upload_to="projects/",null=True,blank=True)
-    description=models.CharField(max_length=200,null=True,blank=True)
+    short_desc=models.CharField(max_length=500,null=True,blank=True)
+    description=models.CharField(max_length=1000,null=True,blank=True)
     source_code_link=models.CharField(max_length=200,null=True,blank=True)
     view_link=models.CharField(max_length=200,null=True,blank=True)
     created_at=models.DateTimeField(auto_now_add=True)
+    priority = models.PositiveIntegerField(default=0,null=True)
     stacks = models.ManyToManyField(Stack,null=True,blank=True)
+    is_visible = models.BooleanField(default=True,null=True,blank=True)
 
 
     def __str__(self):
