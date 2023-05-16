5ab3ead925a3145b93b17e123f66a78a5ed2b6d1
muhammed-mizaj <mdprgamer@gmail.com>
2023-05-16 12:14:14 +0530

    changed to serverless db

--- md_backend/md_backend/settings.py
+++ md_backend/md_backend/settings.py
@@ -81,13 +81,22 @@ WSGI_APPLICATION = 'md_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neondb',
+        'USER': 'mdprgamer',
+        'PASSWORD': 'NqdGMfBx6K3I',
+        'HOST': 'ep-tight-bush-796095.us-east-2.aws.neon.tech',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
